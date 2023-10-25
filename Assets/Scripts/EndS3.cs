8d362ca4cc5ce310d7678f9ec235edb17f1ba5e1
ArnauOliveras <aoliverasf@edu.tecnocampus.cat>
2023-10-25 17:19:37 +0200

    ugu

--- Assets/Scripts/EndS3.cs
+++ Assets/Scripts/EndS3.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndS3 : MonoBehaviour
 {
     public TextNode[] TextEnd;
+    bool end = false;
 
     private void OnTriggerEnter(Collider other)
     {
         GameObject.FindGameObjectWithTag("GameManeger").GetComponent<GameManeger>().TM.SetNodesText(TextEnd);
+        end = true;
+    }
+    private void Update()
+    {
+        if (end && !GameObject.FindGameObjectWithTag("GameManeger").GetComponent<GameManeger>().TM.isTalking)
+        {
+            SceneManager.LoadScene("4_HouseCurandero");
+        }
     }
 }
